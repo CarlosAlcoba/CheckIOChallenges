b3a50baeb5fe837326169579b767435113153ac3
Carlos <kikealcoba@gmail.com>
2022-10-08 23:02:42 -0400

    School Work

---
+++ t03.py
@@ -0,0 +1,7 @@
+user_date = int(input("Enter a date in the format DDMMYYYY: "))
+day = user_date // 1000000
+month = (user_date // 10000) % 100
+year = user_date % 100000
+
+print()
+print(f"The reformatted date: {year}/{month}/{day}")
